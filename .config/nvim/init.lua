ad15312e127bf7173d1882c74ac1c8178852a543
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2025-07-10 20:23:03 +0200

    nvim: reformat keybindings
    Break them up over multiple lines.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,10 +84,26 @@ require("lazy").setup {
         scope = { enabled = true },
       },
       keys = {
-        { "<leader><space>", function() Snacks.picker.smart() end,   desc = "Smart find files" },
-        { "<leader>f",       function() Snacks.picker.files() end,   desc = "Find files" },
-        { "<leader>b",       function() Snacks.picker.buffers() end, desc = "Buffers" },
-        { "<leader>/",       function() Snacks.picker.grep() end,    desc = "Grep" },
+        {
+          "<leader><space>",
+          function() Snacks.picker.smart() end,
+          desc = "Smart find files"
+        },
+        {
+          "<leader>f",
+          function() Snacks.picker.files() end,
+          desc = "Find files"
+        },
+        {
+          "<leader>b",
+          function() Snacks.picker.buffers() end,
+          desc = "Buffers"
+        },
+        {
+          "<leader>/",
+          function() Snacks.picker.grep() end,
+          desc = "Grep"
+        },
       },
     },
 
@@ -133,11 +149,36 @@ require("lazy").setup {
       opts = {},
       -- stylua: ignore
       keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        {
+          "s",
+          mode = { "n", "x", "o" },
+          function() require("flash").jump() end,
+          desc = "Flash"
+        },
+        {
+          "S",
+          mode = { "n", "x", "o" },
+          function() require("flash").treesitter() end,
+          desc = "Flash Treesitter"
+        },
+        {
+          "r",
+          mode = "o",
+          function() require("flash").remote() end,
+          desc = "Remote Flash"
+        },
+        {
+          "R",
+          mode = { "o", "x" },
+          function() require("flash").treesitter_search() end,
+          desc = "Treesitter Search"
+        },
+        {
+          "<c-s>",
+          mode = { "c" },
+          function() require("flash").toggle() end,
+          desc = "Toggle Flash Search"
+        },
       },
     },
 
