399a1a94596f194634a4b57667a7e10f57d258ec
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2025-07-10 20:19:59 +0200

    nvim: fix typo in comment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,7 +10,7 @@ vim.g.have_nerd_font = true
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
--- Show relative line number
+-- Show relative line numbers
 vim.o.number = true
 vim.o.relativenumber = true
 
