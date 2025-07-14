bf6d5b35609418925357d21baabefdefea714757
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2025-07-14 16:56:55 +0200

    nvim: extract plugin list variable

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
+--------------------------------------------------------------------------------
+-- OPTIONS
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -26,7 +29,199 @@ end)
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
--- Bootstrap lazy.nvim
+--------------------------------------------------------------------------------
+-- PLUGINS
+
+local plugins = {
+  -- Theme
+  -- https://github.com/folke/tokyonight.nvim
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup {
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+        },
+      }
+      vim.cmd.colorscheme "tokyonight-night"
+    end
+  },
+
+  -- Snacks - a collection of small quality-of-life plugins
+  -- https://github.com/folke/snacks.nvim
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      dashboard = { enabled = true },
+      picker = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      notify = { enabled = true },
+      bigfile = { enabled = true },
+      quickfile = { enabled = true },
+      indent = { enabled = true },
+      scope = { enabled = true },
+    },
+    keys = {
+      {
+        "<leader><space>",
+        function() Snacks.picker.smart() end,
+        desc = "Smart find files"
+      },
+      {
+        "<leader>f",
+        function() Snacks.picker.files() end,
+        desc = "Find files"
+      },
+      {
+        "<leader>b",
+        function() Snacks.picker.buffers() end,
+        desc = "Buffers"
+      },
+      {
+        "<leader>/",
+        function() Snacks.picker.grep() end,
+        desc = "Grep"
+      },
+    },
+  },
+
+  -- Show available keybindings as you type
+  -- https://github.com/folke/which-key.nvim
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- Single column list in the bottom right corner
+      preset = "helix",
+      -- Disable icons
+      icons = { mappings = false },
+    },
+  },
+
+  -- Collection of various small independent plugins/modules
+  -- https://github.com/echasnovski/mini.nvim
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      -- File type icons
+      -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-icons.md
+      require("mini.icons").setup()
+
+      -- Minimal statusline
+      -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-statusline.md
+      local statusline = require "mini.statusline"
+      statusline.setup()
+      statusline.section_location = function()
+        -- Set location to LINE:COLUMN
+        return "%2l:%-2v"
+      end
+    end,
+  },
+
+  -- Jump around
+  -- https://github.com/folke/flash.nvim
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function() require("flash").jump() end,
+        desc = "Flash"
+      },
+      {
+        "S",
+        mode = { "n", "x", "o" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter"
+      },
+      {
+        "r",
+        mode = "o",
+        function() require("flash").remote() end,
+        desc = "Remote Flash"
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc = "Treesitter Search"
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc = "Toggle Flash Search"
+      },
+    },
+  },
+
+  -- Treesitter
+  -- https://github.com/nvim-treesitter/nvim-treesitter
+  --
+  -- You should have these packages installed:
+  -- https://archlinux.org/groups/x86_64/tree-sitter-grammars/
+  -- https://archlinux.org/packages/extra/x86_64/tree-sitter-cli/
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs", -- Sets main module to use for opts
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "css",
+        "diff",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "html",
+        "ini",
+        "json",
+        "json5",
+        "kdl",
+        "lua",
+        "luadoc",
+        "markdown_inline",
+        "markdown",
+        "nu",
+        "query",
+        "ron",
+        "rust",
+        "toml",
+        "wgsl",
+      },
+      -- Auto install languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = true },
+    },
+    -- There are additional nvim-treesitter modules that you can use to interact
+    -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --
+    -- - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    -- - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    -- - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  },
+}
+
+-- Bootstrap lazy.nvim package manager
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -45,196 +240,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Manage plugins
 require("lazy").setup {
-  spec = {
-    -- Theme
-    -- https://github.com/folke/tokyonight.nvim
-    {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        require("tokyonight").setup {
-          styles = {
-            comments = { italic = false },
-            keywords = { italic = false },
-          },
-        }
-        vim.cmd.colorscheme "tokyonight-night"
-      end
-    },
-
-    -- Snacks - a collection of small quality-of-life plugins
-    -- https://github.com/folke/snacks.nvim
-    {
-      "folke/snacks.nvim",
-      priority = 1000,
-      lazy = false,
-      ---@type snacks.Config
-      opts = {
-        dashboard = { enabled = true },
-        picker = { enabled = true },
-        input = { enabled = true },
-        notifier = { enabled = true },
-        notify = { enabled = true },
-        bigfile = { enabled = true },
-        quickfile = { enabled = true },
-        indent = { enabled = true },
-        scope = { enabled = true },
-      },
-      keys = {
-        {
-          "<leader><space>",
-          function() Snacks.picker.smart() end,
-          desc = "Smart find files"
-        },
-        {
-          "<leader>f",
-          function() Snacks.picker.files() end,
-          desc = "Find files"
-        },
-        {
-          "<leader>b",
-          function() Snacks.picker.buffers() end,
-          desc = "Buffers"
-        },
-        {
-          "<leader>/",
-          function() Snacks.picker.grep() end,
-          desc = "Grep"
-        },
-      },
-    },
-
-    -- Show available keybindings as you type
-    -- https://github.com/folke/which-key.nvim
-    {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- Single column list in the bottom right corner
-        preset = "helix",
-        -- Disable icons
-        icons = { mappings = false },
-      },
-    },
-
-    -- Collection of various small independent plugins/modules
-    -- https://github.com/echasnovski/mini.nvim
-    {
-      "echasnovski/mini.nvim",
-      config = function()
-        -- File type icons
-        -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-icons.md
-        require("mini.icons").setup()
-
-        -- Minimal statusline
-        -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-statusline.md
-        local statusline = require "mini.statusline"
-        statusline.setup()
-        statusline.section_location = function()
-          -- Set location to LINE:COLUMN
-          return "%2l:%-2v"
-        end
-      end,
-    },
-
-    -- Jump around
-    -- https://github.com/folke/flash.nvim
-    {
-      "folke/flash.nvim",
-      event = "VeryLazy",
-      ---@type Flash.Config
-      opts = {},
-      -- stylua: ignore
-      keys = {
-        {
-          "s",
-          mode = { "n", "x", "o" },
-          function() require("flash").jump() end,
-          desc = "Flash"
-        },
-        {
-          "S",
-          mode = { "n", "x", "o" },
-          function() require("flash").treesitter() end,
-          desc = "Flash Treesitter"
-        },
-        {
-          "r",
-          mode = "o",
-          function() require("flash").remote() end,
-          desc = "Remote Flash"
-        },
-        {
-          "R",
-          mode = { "o", "x" },
-          function() require("flash").treesitter_search() end,
-          desc = "Treesitter Search"
-        },
-        {
-          "<c-s>",
-          mode = { "c" },
-          function() require("flash").toggle() end,
-          desc = "Toggle Flash Search"
-        },
-      },
-    },
-
-    -- Treesitter
-    -- https://github.com/nvim-treesitter/nvim-treesitter
-    --
-    -- You should have these packages installed:
-    -- https://archlinux.org/groups/x86_64/tree-sitter-grammars/
-    -- https://archlinux.org/packages/extra/x86_64/tree-sitter-cli/
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      main = "nvim-treesitter.configs", -- Sets main module to use for opts
-      opts = {
-        ensure_installed = {
-          "bash",
-          "c",
-          "cpp",
-          "css",
-          "diff",
-          "git_config",
-          "git_rebase",
-          "gitcommit",
-          "gitignore",
-          "html",
-          "ini",
-          "json",
-          "json5",
-          "kdl",
-          "lua",
-          "luadoc",
-          "markdown_inline",
-          "markdown",
-          "nu",
-          "query",
-          "ron",
-          "rust",
-          "toml",
-          "wgsl",
-        },
-        -- Auto install languages that are not installed
-        auto_install = true,
-        highlight = {
-          enable = true,
-        },
-        indent = { enable = true },
-      },
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      -- - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      -- - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      -- - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-    },
-  },
-
+  spec = plugins,
   -- Theme that will be used when installing plugins
   install = { colorscheme = { "tokyonight-night" } },
 }
