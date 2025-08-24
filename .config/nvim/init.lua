167116b0310c09570403a132a322403ce913b165
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2025-08-24 18:35:13 +0200

    nvim: install treesitter-textobjects and treesitter-context

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -162,12 +162,10 @@ kit.init_lazy {
       },
       indent = { enable = true },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    -- - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    -- - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    -- - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "nvim-treesitter/nvim-treesitter-context",
+    }
   },
 
   -- Properly configures Lua Language Server for editing the Neovim config
