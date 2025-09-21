abd0b5b5c8ddeb9331400880fec3b93fb4b63651
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2025-09-21 10:43:56 +0200

    nvim: prefix mini.surround mappings with `gs`
    This is what LazyVim does.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -327,18 +327,15 @@ local plugins = {
   -- https://github.com/nvim-mini/mini.pairs
   {
     'nvim-mini/mini.surround',
-    enabled = false,
+    enabled = true,
     opts = {
       mappings = {
-        add = 'ma', -- Add surrounding in Normal and Visual modes
-        delete = 'md', -- Delete surrounding
-        find = 'mf', -- Find surrounding (to the right)
-        find_left = 'mF', -- Find surrounding (to the left)
-        highlight = 'mh', -- Highlight surrounding
-        replace = 'mr', -- Replace surrounding
-
-        suffix_last = 'l', -- Suffix to search with "prev" method
-        suffix_next = 'n', -- Suffix to search with "next" method
+        add = 'gsa',
+        delete = 'gsd',
+        find = 'gsf',
+        find_left = 'gsF',
+        highlight = 'gsh',
+        replace = 'gsr',
       },
     },
   },
