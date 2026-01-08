210b5158626a29724331d4c5f7913e1bcd716200
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2026-01-08 11:05:05 +0100

    Revert "nvim: give "koda" theme a go"
    This reverts commit 91b81b75a40efd3712254ed36dbaaf20c87fa2ec.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -628,30 +628,20 @@ local plugins = {
   -- TODO: Replace this with my own theme.
   --
   -- https://github.com/folke/tokyonight.nvim
-  -- {
-  --   'folke/tokyonight.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     styles = {
-  --       comments = { italic = false },
-  --       keywords = { italic = false },
-  --     },
-  --   },
-  -- },
-
   {
-    "oskarnurm/koda.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- require("koda").setup({ transparent = true })
-      vim.cmd("colorscheme koda")
-    end,
-  }
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+      },
+    },
+  },
 }
 
-local theme = 'koda'
+local theme = 'tokyonight-night'
 
 kit.init_lazy {
   spec = plugins,
