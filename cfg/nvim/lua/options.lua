7ca7f5bc09e56e52c223b5462f9102fe54ac987e
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2026-05-17 20:52:11 +0200

    nvim: tweak options

--- cfg/nvim/lua/options.lua
+++ cfg/nvim/lua/options.lua
@@ -8,7 +8,7 @@ vim.opt.termguicolors = true
 -- Use the system clipboard.
 vim.opt.clipboard = "unnamedplus"
 
--- Disable the swap file as it"s a source of pointless error messages.
+-- Disable the swap file as it's a source of pointless error messages.
 vim.opt.swapfile = false
 
 -- Auto reload a file when it changes on disk.
@@ -24,17 +24,20 @@ vim.opt.signcolumn = "yes"
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Highlight the line that the cursor is on. 
+-- Highlight the current line.
 vim.opt.cursorline = true
 
--- Minimum number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+-- Minimum number of lines and columns to keep around the cursor.
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 8
 
 -- Indent using two spaces.
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 -- Open new vertical split below.
 vim.opt.splitbelow = true
