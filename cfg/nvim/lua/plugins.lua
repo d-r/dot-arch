85172c143e7d7707002f03d7a42ec2010c40a4dc
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2026-05-20 22:51:09 +0200

    nvim: move plugin specs to separate file

--- cfg/nvim/lua/plugins.lua
+++ cfg/nvim/lua/plugins.lua
@@ -1,49 +1,29 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  spec = {
-    -- Show available keybindings in a popup as you type.
-    -- https://github.com/folke/which-key.nvim
-    {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      opts = {
-        preset = 'helix',
-      },
-      keys = {
-        {
-          "<leader>?",
-          function()
-            require("which-key").show({ global = false })
-          end,
-          desc = "Buffer Local Keymaps (which-key)",
-        },
-      },
+return {
+  -- Show available keybindings in a popup as you type.
+  -- https://github.com/folke/which-key.nvim
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      preset = 'helix',
     },
-
-    -- Automatically install and enable tree-sitter parsers.
-    -- https://github.com/arborist-ts/arborist.nvim
-    {
-      "arborist-ts/arborist.nvim",
-      opts = {
-        prefer_wasm = false,
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
       },
     },
   },
-  -- Settings go here. See the documentation for more details.
-})
 
+  -- Automatically install and enable tree-sitter parsers.
+  -- https://github.com/arborist-ts/arborist.nvim
+  {
+    "arborist-ts/arborist.nvim",
+    opts = {
+      prefer_wasm = false,
+    },
+  },
+}
