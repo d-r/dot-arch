ee4fce2b69a0a1ee457e23ab98c2d0d6dd78e906
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2026-06-18 23:37:24 +0200

    nvim: move everything into init.lua

--- cfg/nvim/lua/binds.lua
+++
@@ -1,5 +0,0 @@
--- Unbind Ctrl+Z to avoid constantly suspending Neovim by accident.
-vim.keymap.set({ "n", "i", "v" }, "<C-z>", "<Nop>")
-
--- Ctrl+S to save.
-vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>")
