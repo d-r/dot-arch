9be9f540ea9c46ad4584ec5b9181e8162e2afa8d
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2026-06-18 23:22:49 +0200

    nvim: bind Ctrl+S

--- cfg/nvim/lua/binds.lua
+++ cfg/nvim/lua/binds.lua
@@ -1,2 +1,5 @@
 -- Unbind Ctrl+Z to avoid constantly suspending Neovim by accident.
 vim.keymap.set({ "n", "i", "v" }, "<C-z>", "<Nop>")
+
+-- Ctrl+S to save.
+vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr>")
