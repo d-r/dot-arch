de97542509038ef319a4d7d43a253e22ba176e5b
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2026-06-27 20:58:16 +0200

    nvim: fix broken lua_ls config

--- cfg/nvim/lsp/lua_ls.lua
+++ cfg/nvim/lsp/lua_ls.lua
@@ -1,7 +1,42 @@
+-- https://github.com/luals/lua-language-server
+-- https://github.com/LuaLS/lua-language-server/wiki/Settings
+---@type vim.lsp.Config
 return {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = {
+    '.luarc.json',
+    '.luarc.jsonc',
+    '.luacheckrc',
+    '.stylua.toml',
+    'stylua.toml',
+    'selene.toml',
+    'selene.yml',
+    '.git',
+  },
   settings = {
     Lua = {
-      runtime = { version = "LuaJIT" },
+      runtime = { version = 'LuaJIT' },
+      telemetry = { enable = false },
+      diagnostics = {
+        globals = { 'vim', 'require', 'pcall', 'pairs' },
+      },
+      workspace = {
+        library = {
+          vim.api.nvim_get_runtime_file('', true),
+          vim.fn.stdpath 'data' .. '/lazy',
+          '${3rd}/luv/library',
+        },
+        checkThirdParty = false,
+      },
+      completion = {
+        workspaceWord = true,
+        callSnippet = 'Replace',
+      },
+      hint = {
+        enable = true,
+      },
+      format = { enable = false }, -- Leave formatting to stylua
     },
   },
 }
